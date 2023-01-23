3c4b9ca0dbbdce3342e6285014f2f810df72fb7e
Ignat Mustafaev <mustafaev@siberian.pro>
2023-01-23 16:03:17 +0700

    fixes

---
+++ app/src/main/java/prank/broken/screen/realistic/crack/cracking/sounds/wallpaper/realistic/cracked/ui/HairClipperMachineFragment.kt
@@ -0,0 +1,7 @@
+package prank.broken.screen.realistic.crack.cracking.sounds.wallpaper.realistic.cracked.ui
+
+import androidx.fragment.app.Fragment
+import prank.broken.screen.realistic.crack.cracking.sounds.wallpaper.realistic.cracked.R
+
+class HairClipperMachineFragment : Fragment(R.layout.fragment_hair_clipper_machine) {
+}
---
+++ app/src/main/java/prank/broken/screen/realistic/crack/cracking/sounds/wallpaper/realistic/cracked/ui/ScreenSettingsFragment.kt
@@ -0,0 +1,7 @@
+package prank.broken.screen.realistic.crack.cracking.sounds.wallpaper.realistic.cracked.ui
+
+import androidx.fragment.app.Fragment
+import prank.broken.screen.realistic.crack.cracking.sounds.wallpaper.realistic.cracked.R
+
+class ScreenSettingsFragment : Fragment(R.layout.fragment_screen_settings) {
+}
